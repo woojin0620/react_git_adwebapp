23917648e914217214e53cbf9408419f47d04f77
woojin <tbvc@naver.com>
2022-01-25 17:30:40 +0900

    -a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Navi  from './nav';
+import Carousel from './carousel';
+import Gallery from './gallery';
+import './main.css'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navi></Navi>
+        <Carousel></Carousel>
+        <Gallery propid='gallery' proptitle='' propfolder='gallery'></Gallery>
       </header>
     </div>
   );
